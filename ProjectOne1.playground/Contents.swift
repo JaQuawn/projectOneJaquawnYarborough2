fbbbe22411b64a61a5f1b0ceb9ce43ad11dbea0d
jaquawn <jd23@hawaii.edu>
2018-02-06 19:13:56 -1000

    need help with challenge 2

--- ProjectOne1.playground/Contents.swift
+++ ProjectOne1.playground/Contents.swift
@@ -3,3 +3,65 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//variable coding challenge #1
+
+var firstString = " JaQuawn will become a multimillionaire "
+
+var secondString = " if he has 7 forms of income "
+
+var thirdString = firstString + secondString
+
+print (thirdString)
+
+
+
+//variable coding challenge #2
+
+let fahrenheit = " (50-32) "
+
+var celsius = " 5/9"
+
+var  celsisus = 5/9 * (50-32)
+
+
+
+
+// Boolen variable coding challenge #3
+
+var linestate =  "equal to"
+print ( linestate)
+
+var numberOne = " 6 "
+var numberTwo = "  9 "
+var numberThree = numberOne == numberTwo
+
+
+
+
+// is statement coding challenge #1
+
+var moneyInvest = " i save money "
+if moneyInvest == " i've been saving all my money"
+{
+    print ( " i can retire well")
+}
+
+
+if  moneyInvest == " i have not been saving all my money"
+{
+    print ( " i can retire well")
+    
+}
+
+if moneyInvest == " i've been saving all my money"
+{
+    print (" i will not retire well")
+}
+else
+{
+    print (" im not saving my money")
+}
+
+
+
